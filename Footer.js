468768ec2750d764d910eec42bbfb2d250b3bb33
sahi2slow <69285569+sahi2slow@users.noreply.github.com>
2020-08-10 13:18:36 -0700

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -10,13 +10,17 @@ const Footer = () => {
                 <a href = "https://www.facebook.com/">
                     <img src = "https://1000logos.net/wp-content/uploads/2016/11/Facebook-Logo.png" className = "social-logo"></img>
                 </a>
-                <a href = "https://www.instagram.com/">
+                <a href = "https://www.instagram.com/ybct_saveslives">
                     <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/240px-Instagram_logo_2016.svg.png" className = "social-logo"></img>
                 </a>
                 <a href = "https://www.youtube.com/">
                     <img src = "https://cdn.worldvectorlogo.com/logos/youtube-icon.svg" className = "social-logo"></img>
                 </a>
-                <p>Email</p>
+                <hr />
+                <p>Email us at:</p>
+                <a href = "mailto: youthbleedingcontrol@gmail.com">
+                    <h4>youthbleedingcontrol@gmail.com</h4>
+                </a>
             </div>
         </div>
     )
