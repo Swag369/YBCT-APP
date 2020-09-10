9fd3bb714b6e911bc97c535334110ac88749e931
sahi2slow <69285569+sahi2slow@users.noreply.github.com>
2020-09-09 18:35:08 -0700

    Updated footer

--- Footer.js
+++ Footer.js
@@ -2,8 +2,10 @@ import React from "react"
 
 const Footer = () => {
     return (
-       <div style = {{backgroundColor: "", display: "block", height: "50", fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"}}>
-           <a style = {{color: "red", textAlign: "center", }} href = "mailto:youthbleedingcontrol@gmail.com">Email</a>
+       <div style = {{backgroundColor: "whitesmoke", display: "block", height: "100", fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif", width: "100%", float: "left", textAlign: "center"}}>
+           <h1 style = {{color: "black", fontSize: 30}}>Interested in learning more? <a style = {{color: "red", float: "center", fontSize: 30}} href = "mailto:youthbleedingcontrol@gmail.com">Email Us</a></h1>
+           <hr />
+           <p style = {{fontSize: 20}}>Copyright © 2020 Youth Bleeding Control Training. All Rights Reserved.</p>
        </div>
     )
 }
