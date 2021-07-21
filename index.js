5060aaa53e4e2b785f6ecc3d9a5789198518c810
Swag369 <86703988+Swag369@users.noreply.github.com>
2021-07-20 23:29:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+//adding my new account here (satteliteboi->swag369)
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
